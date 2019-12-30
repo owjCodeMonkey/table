0451bca2a96b7a76c48ac70a2774e8b8c3d5b730
owenqu(区文骏) <owenqu@futunn.com>
2019-12-30 11:43:47 +0800

    done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <el-table :data="tableData" height="800" border style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      tableData: []
+    };
+  },
+  async mounted() {
+    try {
+      //后端写个api返回数组数据
+      this.tableData = await axios.get("/api/getData");
+    } catch (e) {
+      //示例
+      this.tableData = Array(100).fill({
+        date: "2016-05-03",
+        name: "王小虎",
+        address: "上海市普陀区金沙江路 1518 弄"
+      });
+    }
   }
-}
+};
 </script>
